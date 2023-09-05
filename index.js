aa103b5647b02e70b6d23d7120977c898085d188
Tyler Graf <tygraf13@gmail.com>
2023-09-05 15:10:09 -0500

    Assignment-1-JS

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(src, bottom, left, position) {
+let newImage = document.createElement('img');
+newImage.src = src;
+newImage.style.position = position;
+newImage.style.left = left;
+newImage.style.bottom = bottom;
+document.body.append(newImage);
+return newImage
+}
+
+
+newImage('assets/green-character.gif', '100px', '100px', 'fixed');
+newImage('assets/pine-tree.png', '200px', '450px', 'fixed');
+newImage('assets/tree.png', '300px', '200px', 'fixed');
+newImage('assets/pillar.png', '100px', '350px', 'fixed');
+newImage('assets/crate.png', '200px', '150px', 'fixed');
+newImage('assets/well.png', '425px', '500px', 'fixed');
+
+function newItem(src, bottom, left, position) {
+    let item = newImage(src, bottom, left, position)
+
+    item.addEventListener('dblclick', () => {
+        item.remove()
+    })
+}
+
+newItem('assets/sword.png', '405px', '500px', 'fixed')
+newItem('assets/shield.png', '165px', '185px', 'fixed')
+newItem('assets/staff.png', '600px', '100px', 'fixed')
